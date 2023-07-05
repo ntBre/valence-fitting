f21afce1398149dcab5c0bbec1422413e2ad3323
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-05 13:36:11 -0400

    port the split torsions from pavan's FF file to 2.1.0

--- 01_generate-forcefield/generate-forcefield.py
+++ 01_generate-forcefield/generate-forcefield.py
@@ -1,4 +1,25 @@
 import click
+from dataclasses import dataclass
+from openff.toolkit import ForceField
+from openff.toolkit.typing.engines.smirnoff.parameters import (
+    ParameterType,
+)
+
+TORSIONS = "ProperTorsions"
+
+
+@dataclass
+class Param:
+    param: ParameterType
+    after: str
+
+
+def get_smirks(params):
+    return [p.smirks for p in params]
+
+
+def by_smirks(smirks):
+    return dict(smirks=smirks)
 
 
 @click.command()
@@ -20,9 +41,40 @@ def download_force_field(
     output_path: str,
     force_field_name: str = "openff-2.1.0.offxml",
 ):
-    from openff.toolkit import ForceField
+    sage20 = ForceField("openff-2.0.0.offxml")
+    tors20 = sage20.get_parameter_handler(TORSIONS)
 
+    pavan = ForceField("force-field.offxml", allow_cosmetic_attributes=True)
+    torspv = pavan.get_parameter_handler(TORSIONS)
+
+    ids20 = get_smirks(tors20.parameters)
+    idspv = get_smirks(torspv.parameters)
+
+    set20 = set(ids20)
+    setpv = set(idspv)
+
+    # - only in pavan's set
+    only_pavan = setpv.difference(set20)
+
+    # gather the parameters only found in pavan's set
+    new_params = []
+    for i, param in enumerate(torspv.parameters):
+        if param.smirks in only_pavan:
+            new_params.append(Param(param, torspv.parameters[i - 1].smirks))
+
+    # use 2.1 as the base for the output force field
     force_field = ForceField(force_field_name)
+    h = force_field.get_parameter_handler(TORSIONS)
+
+    # add the new parameters
+    for p in new_params:
+        # avoid duplicate parameter error
+        if not h.get_parameter(by_smirks(p.param.smirks)):
+            # ensure unique id
+            if p.param.id in [p.id for p in h.parameters]:
+                p.param.id += "x"
+            h.add_parameter(parameter=p.param, after=p.after)
+
     force_field.to_file(output_path)
 
 

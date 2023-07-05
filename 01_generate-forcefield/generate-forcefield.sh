73fde35adcdb7be6acc64a1dc9f86a820a449053
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-05 13:36:11 -0400

    port the split torsions from pavan's FF file to 2.1.0

--- 01_generate-forcefield/generate-forcefield.sh
+++ 01_generate-forcefield/generate-forcefield.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-
-source ~/.bashrc
-micromamba activate fb-195-tk-014-py310
-
 python generate-forcefield.py                               \
     --force-field-name          "openff-2.1.0.offxml"       \
     --output                    "output/initial-force-field-openff-2.1.0.offxml"

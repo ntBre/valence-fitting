2d188e0bbbb80783de9f5b1b7e32f073711ea337
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-07 11:26:50 -0400

    generate forcebalance inputs

--- 04_fit-forcefield/create-fb-inputs.sh
+++ 04_fit-forcefield/create-fb-inputs.sh
@@ -2,11 +2,11 @@
 
 python create-fb-inputs.py                                                                          \
     --tag                       "fb-fit"                                                            \
-    --optimization-dataset      "../02_curate-data/output/optimization-training-set.json"           \
-    --torsion-dataset           "../02_curate-data/output/torsion-training-set.json"                \
+    --optimization-dataset      "../02_curate-data/output/combined-opt.json"           \
+    --torsion-dataset           "../02_curate-data/output/combined-td.json"                \
     --forcefield                "../03_generate-initial-ff/output/initial-force-field-msm.offxml"   \
-    --valence-to-optimize       "../02_curate-data/output/training-valence-smirks.json"             \
-    --torsions-to-optimize      "../02_curate-data/output/training-torsion-smirks.json"             \
+    --valence-to-optimize       "../02_curate-data/output/combined-opt-smirks.json"             \
+    --torsions-to-optimize      "../02_curate-data/output/combined-td-smirks.json"             \
     --smiles-to-exclude         "smiles-to-exclude.dat"                                             \
     --smarts-to-exclude         "smarts-to-exclude.dat"                                             \
     --max-iterations            1                                                                   \

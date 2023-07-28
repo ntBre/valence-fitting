0330c3316442b6c92f5d1ac98c94ffb12315be27
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-28 10:18:52 -0400

    update job submission scripts to what I've been using

--- fit-sage/scripts/hpc3_master.sh
+++ fit-sage/scripts/hpc3_master.sh
@@ -9,27 +9,13 @@
 #SBATCH --export ALL
 #SBATCH --mail-user=MY_USERNAME@uci.edu
 #SBATCH --constraint=fastscratch
-
+#SBATCH -o master.out
+#SBATCH -e master.err
 
 rm -rf /tmp/$SLURM_JOB_NAME
 source $HOME/.bashrc
-eval "$(/opt/apps/anaconda/2020.07/bin/conda shell.bash hook)"
 
-# special code to copy conda environment over
-# relies on having set up a conda environment and compressed it
-
-CONDA_ENVIRONMENT_NAME="fb-195-tk-014-py310"
-COMPRESSED_CONDA_ENVIRONMENT="/dfs4/.../${CONDA_ENVIRONMENT_NAME}.tar.gz"
-export SLURM_TMPDIR=/tmp
-export TMPDIR=$SLURM_TMPDIR/$SLURM_JOB_NAME
-
-mkdir  -p  $TMPDIR
-rsync -avzIi $COMPRESSED_CONDA_ENVIRONMENT $TMPDIR
-mkdir  -p  $TMPDIR/$CONDA_ENVIRONMENT_NAME
-cd $TMPDIR
-tar xf "${CONDA_ENVIRONMENT_NAME}.tar.gz" -C $CONDA_ENVIRONMENT_NAME
-source "${CONDA_ENVIRONMENT_NAME}/bin/activate"
-echo $(python -V)
+mamba activate valence-fitting
 
 rsync  -avzIi  $SLURM_SUBMIT_DIR/optimize.in  $SLURM_TMPDIR/$SLURM_JOB_NAME
 rsync  -avzIi  $SLURM_SUBMIT_DIR/targets.tar.gz  $SLURM_TMPDIR/$SLURM_JOB_NAME
@@ -46,7 +32,9 @@ export MKL_NUM_THREADS=1
 
 if ForceBalance.py optimize.in ; then
    tar -czvf optimize.tmp.tar.gz optimize.tmp
-   rsync  -avzIi --exclude="optimize.tmp" --exclude="optimize.bak" --exclude="fb_193*" --exclude="targets*" $TMPDIR/* $SLURM_SUBMIT_DIR > copy.log
+   rsync  -avzIi --exclude="optimize.tmp" --exclude="optimize.bak" \
+	  --exclude="fb_193*" --exclude="targets*" $TMPDIR/* \
+	  $SLURM_SUBMIT_DIR > copy.log
    rm -rf $TMPDIR
 fi
 

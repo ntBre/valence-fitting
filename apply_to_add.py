b65343639b6d503942fb953a6ef4e74671462a5a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-22 16:37:38 -0500

    run apply_to_add and write to initial ff
    start new suffixes from g to avoid conflicts

---
+++ apply_to_add.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from copy import deepcopy
+
+from openff.toolkit import ForceField
+
+base = ForceField("openff-2.1.0.offxml")
+
+with open("to_add.dat") as inp:
+    to_add = [
+        line.strip()
+        for line in inp
+        if not line.startswith("#") and len(line) > 1
+    ]
+
+new_params = defaultdict(list)
+for line in to_add:
+    [pid, smirks] = line.split()
+    new_params[pid].append(smirks)
+
+torsions = base.get_parameter_handler("ProperTorsions")
+
+old = deepcopy(torsions.parameters)
+torsions.parameters.clear()
+
+for t in old:
+    if t.id in new_params:
+        for i, p in enumerate(new_params[t.id]):
+            tmp = deepcopy(t)
+            tmp.id = t.id + "ghijkl"[i]
+            tmp.smirks = p
+            torsions.parameters.append(tmp)
+    else:
+        torsions.parameters.append(t)
+
+base.to_file(
+    "01_generate-forcefield/output/initial-force-field-openff-2.1.0.offxml"
+)

97c6e1021ddf5d4e9206ca616bc8cd36dfe219d3
Brent Westbrook <brentrwestbrook@gmail.com>
2024-03-13 12:41:12 -0400

    update download_opt based on updated download_td

--- 02_curate-data/download_opt.py
+++ 02_curate-data/download_opt.py
@@ -1,16 +1,27 @@
-from qcportal import FractalClient
+import click
 from openff.qcsubmit.results import OptimizationResultCollection
+from qcportal import PortalClient
 
 
-ds_cache = "datasets/core-opt.json"
-opt_datasets = ["OpenFF multiplicity correction optimization set v1.0"]
-
-client = FractalClient()
-dataset = OptimizationResultCollection.from_server(
-    client=client,
-    datasets=opt_datasets,
-    spec_name="default",
+@click.command()
+@click.option("--output", "-o", default="datasets/core-opt.json")
+@click.option(
+    "--dataset",
+    "-d",
+    default="OpenFF multiplicity correction optimization set v1.0",
 )
+def main(output, dataset):
+    td_datasets = [dataset]
 
-with open(ds_cache, "w") as out:
-    out.write(dataset.json(indent=2))
+    client = PortalClient("https://api.qcarchive.molssi.org:443/")
+    dataset = OptimizationResultCollection.from_server(
+        client=client,
+        datasets=td_datasets,
+        spec_name="default",
+    )
+    with open(output, "w") as out:
+        out.write(dataset.json(indent=2))
+
+
+if __name__ == "__main__":
+    main()

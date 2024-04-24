464965bfd66d14edb2521b80223e1dca93bc7aec
Brent Westbrook <brentrwestbrook@gmail.com>
2024-04-24 11:16:27 -0400

    reproduce original torsion multiplicity work on sage 2.0

---
+++ 01_generate-forcefield/multiplicity/main.py
@@ -0,0 +1,104 @@
+import re
+from collections import defaultdict
+from itertools import chain
+from typing import Iterator
+
+from openff.qcsubmit.results import (
+    OptimizationResultCollection,
+    TorsionDriveResultCollection,
+)
+from openff.toolkit import ForceField, Molecule
+from tqdm import tqdm
+
+
+def molecules(smiles: Iterator[str]) -> Iterator[Molecule]:
+    seen = set()
+    for s in smiles:
+        if s not in seen:
+            seen.add(s)
+            yield Molecule.from_mapped_smiles(s, allow_undefined_stereo=True)
+
+
+def bonds(mol: Molecule, i: int, j: int) -> int:
+    "Return the multiplicity of the central bond between atoms i and j"
+
+    def inner(mol, idx):
+        return sum(
+            (
+                1
+                for b in mol.bonds
+                if b.atom1_index == idx or b.atom2_index == idx
+            )
+        )
+
+    # -1 to discard shared central bond
+    a = inner(mol, i) - 1
+    b = inner(mol, j) - 1
+
+    return a * b
+
+
+def load_dataset(ds) -> Iterator[Molecule]:
+    return molecules((e.cmiles for e in next(iter(ds.entries.values()))))
+
+
+def param_sort_key(pid: str):
+    m = re.match(r"t([0-9]+)(a-z)*", pid)
+    return int(m[1]), m[2]
+
+
+ff = ForceField("openff-2.0.0.offxml")
+opt = load_dataset(
+    OptimizationResultCollection.parse_file(
+        "../../02_curate-data/datasets/combined-opt.json"
+    )
+)
+td = load_dataset(
+    TorsionDriveResultCollection.parse_file(
+        "../../02_curate-data/datasets/combined-td.json"
+    )
+)
+mols = chain(opt, td)
+
+res = defaultdict(set)
+for mol in tqdm(mols):
+    labels = ff.label_molecules(mol.to_topology())[0]["ProperTorsions"]
+    for (_, i, j, _), p in labels.items():
+        res[p.id].add(bonds(mol, i, j))
+
+# parameters identified in original torsion multiplicity work on Sage 2.0
+prev = {
+    "t58",
+    "t59",
+    "t60",
+    "t61",
+    "t62",
+    "t71",
+    "t72",
+    "t73",
+    "t74",
+    "t82",
+    "t83",
+    "t115",
+    "t116",
+    "t118",
+    "t119",
+    "t120",
+    "t121",
+    "t122",
+    "t127",
+    "t130",
+    "t132",
+    "t133",
+    "t142",
+    "t143",
+    "t157",
+    "t166",
+    "t167",
+}
+cur = {r for r in res.keys()}
+assert len(cur & prev) == len(prev)
+
+for k, v in sorted(res.items(), key=lambda x: param_sort_key(x[0])):
+    if len(v) > 1:
+        print(k, v)

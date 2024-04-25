3891641a73ef72f3850d5fe065115358b30c5126
Brent Westbrook <brentrwestbrook@gmail.com>
2024-04-25 13:57:39 -0400

    rename load_chembl and parameterize

--- 02_curate-data/frag/main.py
+++ 02_curate-data/frag/main.py
@@ -1,7 +1,7 @@
 from openff.toolkit import Molecule
 
 
-def load_smiles(filename) -> list[Molecule]:
+def load_chembl(filename, max_atoms=80, max_mols=1000) -> list[Molecule]:
     ret = []
     with open(filename) as inp:
         for i, line in enumerate(inp):
@@ -9,9 +9,9 @@ def load_smiles(filename) -> list[Molecule]:
                 continue
             [_chembl_id, cmiles, _inchi, _inchikey] = line.split("\t")
             mol = Molecule.from_smiles(cmiles, allow_undefined_stereo=True)
-            if mol.n_atoms < 80:
+            if mol.n_atoms < max_atoms:
                 ret.append(mol)
-            if len(ret) >= 1000:
+            if len(ret) >= max_mols:
                 break
     return ret
 
@@ -22,4 +22,4 @@ def save_mols(filename, mols):
             print(mol.to_smiles(), file=out)
 
 
-save_mols("1000.smi", load_smiles("chembl_33_chemreps.txt"))
+save_mols("1000.smi", load_chembl("chembl_33_chemreps.txt"))

9eefbd51811dd8c018b73c580107e3440a59ce8d
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-07 13:20:21 -0400

    act like a real test, use list for adding more cases later

--- 02_curate-data/frag/testing.py
+++ 02_curate-data/frag/testing.py
@@ -2,18 +2,21 @@ from openff.toolkit import ForceField, Molecule
 
 from query import find_matches, into_params, mol_from_smiles
 
-ff = ForceField("openff-2.1.0.offxml")
-params = into_params(ff)
 
-offmol = Molecule.from_smiles("CCO")
-rdmol = mol_from_smiles("CCO")
+def test_find_matches():
+    ff = ForceField("openff-2.1.0.offxml")
+    params = into_params(ff)
 
-got = list(sorted(find_matches(params, rdmol).items()))
-want = list(
-    (k, v.id)
-    for k, v in sorted(
-        ff.label_molecules(offmol.to_topology())[0]["ProperTorsions"].items()
-    )
-)
-
-assert got == want
+    for smile in ["CCO"]:
+        offmol = Molecule.from_smiles(smile)
+        rdmol = mol_from_smiles(smile)
+        got = list(sorted(find_matches(params, rdmol).items()))
+        want = list(
+            (k, v.id)
+            for k, v in sorted(
+                ff.label_molecules(offmol.to_topology())[0][
+                    "ProperTorsions"
+                ].items()
+            )
+        )
+        assert got == want, f"got = {got}, want = {want}"

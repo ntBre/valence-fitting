9220e22ef722f9a483bf09476c093a5d33f1c3e5
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-21 12:28:26 -0400

    try computing morgan fingerprint

--- 02_curate-data/frag/parse.py
+++ 02_curate-data/frag/parse.py
@@ -1,15 +1,20 @@
 import json
 
-from query import Match
+from rdkit.Chem import AllChem
+
+from query import Match, mol_from_smiles
 
 with open("out.query.json") as f:
     data: dict[str, dict] = json.load(f)
 
 matches: dict[str, Match] = {k: Match(**v) for k, v in data.items()}
 
+fpgen = AllChem.GetMorganGenerator(radius=3)
+
 for m in matches.values():
     lm = len(m.molecules)
     print(m.pid, m.smirks, lm)
     if lm <= 5:
-        for mol in m.molecules:
-            print("\t", mol)
+        for smile in m.molecules:
+            mol = mol_from_smiles(smile)
+            print("\t", smile, len(fpgen.GetFingerprint(mol)))

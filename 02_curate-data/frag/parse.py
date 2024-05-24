7449cad36943f7407794a9a24a5fe487b5e8d4e3
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-24 12:59:34 -0400

    cluster distance matrices with dbscan

--- 02_curate-data/frag/parse.py
+++ 02_curate-data/frag/parse.py
@@ -1,6 +1,7 @@
 import numpy as np
 from rdkit import DataStructs
 from rdkit.Chem import AllChem
+from sklearn import cluster
 
 from query import mol_from_smiles
 from store import Store
@@ -20,6 +21,9 @@ def tanimoto(fps):
     return ret + ret.T  # copy upper triangle to lower, diag is already zero
 
 
+DB_EPS = 0.7
+DB_MIN_SAMPLES = 1
+
 ff = (
     "../../01_generate-forcefield/output/"
     "initial-force-field-openff-2.1.0.offxml"
@@ -32,6 +36,7 @@ matches = dbff.matches
 print(f"found {len(matches)} matches")
 
 fpgen = AllChem.GetMorganGenerator(radius=3)
+dbscan = cluster.DBSCAN(eps=DB_EPS, min_samples=DB_MIN_SAMPLES)
 
 for m in matches:
     print(m.pid, m.smirks, len(m.molecules))
@@ -40,4 +45,6 @@ for m in matches:
         mol = mol_from_smiles(smile)
         fps.append(fpgen.GetFingerprint(mol))
     dist = tanimoto(fps)
+    clustering = dbscan.fit(dist)
     print(dist)
+    print(clustering.labels_)

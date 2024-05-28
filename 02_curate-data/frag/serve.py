0c30f1b13f661e55a82c7db00f6052b91ed29c2f
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-28 11:04:25 -0400

    draw the molecules

--- 02_curate-data/frag/serve.py
+++ 02_curate-data/frag/serve.py
@@ -1,10 +1,13 @@
 import re
+from dataclasses import dataclass
 
 from flask import Flask, request, send_from_directory
 from jinja2 import Environment, PackageLoader, select_autoescape
 from openff.toolkit import ForceField
+from rdkit import Chem
+from rdkit.Chem.Draw import MolsToGridImage, rdDepictor, rdMolDraw2D
 
-from query import mol_from_smiles
+from query import find_matches, into_params, mol_from_smiles
 from store import Store
 
 app = Flask("serve")
@@ -30,6 +33,22 @@ def pid_sort(pid: str) -> tuple[str, int, str | None]:
     "Return the fields of a ProperTorsion parameter ID as a tuple for sorting"
     t, n, tail = PID_RE.match(pid).groups()
     return (t, int(n), tail)
+
+
+def mol_to_svg(
+    mol: Chem.Mol, width: int, height: int, legend: str, hl_atoms: list[int]
+) -> str:
+    "Return an SVG of `mol`"
+    rdDepictor.SetPreferCoordGen(True)
+    rdDepictor.Compute2DCoords(mol)
+    rdmol = rdMolDraw2D.PrepareMolForDrawing(mol)
+    return MolsToGridImage(
+        [rdmol],
+        useSVG=True,
+        highlightAtomLists=[hl_atoms],
+        subImgSize=(300, 300),
+        molsPerRow=1,
+    )
 
 
 @app.route("/js/<path:name>")
@@ -61,10 +80,17 @@ def index():
     )
 
 
+@dataclass
+class DrawMol:
+    smiles: str
+    natoms: int
+    svg: str
+
+
 @app.route("/param/<pid>")
 def param(pid):
     MAX_DRAW = 50
-    max_draw = request.args.get("max", MAX_DRAW)
+    max_draw = int(request.args.get("max", MAX_DRAW))
     table = Store.quick()
     smiles_list = table.get_smiles_matching(ff, pid)
     mols = []
@@ -73,13 +99,26 @@ def param(pid):
         natoms = mol.GetNumAtoms()
         mols.append((mol, s, natoms))
     mols = sorted(mols, key=lambda k: k[2])  # sort by natoms
+    mol_map = into_params(off)
+
+    draw_mols = []
+    for mol, smiles, _natoms in mols[:max_draw]:
+        matches = find_matches(mol_map, mol)
+        hl_atoms = []
+        for atoms, mpid in matches.items():
+            if mpid == pid:
+                hl_atoms = atoms
+                break
+        svg = mol_to_svg(mol, 300, 300, "", hl_atoms)
+        draw_mols.append(DrawMol(smiles, natoms, svg))
+
     smarts = pid_to_smarts[pid]  # TODO error page on missing
     total_mols = len(mols)
     template = env.get_template("param.html")
     return template.render(
         smarts=smarts,
         pid=pid,
-        mols=mols,
-        cur_mols=len(mols),
+        mols=draw_mols,
+        cur_mols=len(draw_mols),
         total_mols=total_mols,
     )

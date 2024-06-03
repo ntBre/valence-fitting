e8140660d29ae9128aa82cef3ea7af284961e0ff
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-03 17:37:27 -0400

    update mult/main to report smiles matching each multiplicity
    and save the results

--- 01_generate-forcefield/multiplicity/main.py
+++ 01_generate-forcefield/multiplicity/main.py
@@ -48,31 +48,40 @@ def param_sort_key(pid: str):
 
 
 def main():
-    ff = ForceField(
-        "01_generate-forcefield/output/initial-force-field-openff-2.1.0.offxml"
-    )
+    # for original splits
+    ff = ForceField("../openff-2.2.0-rc1.offxml")
+
+    # for checking that splits worked - should output nothing
+    # ff = ForceField("../output/initial-force-field-openff-2.1.0.offxml")
+
     opt = load_dataset(
         OptimizationResultCollection.parse_file(
-            "02_curate-data/datasets/combined-opt.json"
+            "../../02_curate-data/datasets/combined-opt.json"
         )
     )
     td = load_dataset(
         TorsionDriveResultCollection.parse_file(
-            "02_curate-data/datasets/combined-td.json"
+            "../../02_curate-data/datasets/combined-td.json"
         )
     )
     bench = load_dataset(
         OptimizationResultCollection.parse_file(
-            "../benchmarking/datasets/industry.json"
+            "../../../benchmarking/datasets/industry.json"
         )
     )
     mols = chain(opt, td, bench)
 
+    def def_mol_dict():
+        return defaultdict(set)
+
     res = defaultdict(set)
+    mol_res = defaultdict(def_mol_dict)
     for mol in tqdm(mols, total=12475):
         labels = ff.label_molecules(mol.to_topology())[0]["ProperTorsions"]
         for (_, i, j, _), p in labels.items():
-            res[p.id].add(bonds(mol, i, j))
+            mult = bonds(mol, i, j)
+            res[p.id].add(mult)
+            mol_res[p.id][mult].add(mol.to_smiles())
 
     h = ff.get_parameter_handler("ProperTorsions")
     for k, v in sorted(res.items(), key=lambda x: param_sort_key(x[0])):
@@ -83,6 +92,14 @@ def main():
         ]:  # skip k=0 torsions
             p = h.get_parameter(dict(id=k))[0]
             print(k, v, p.smirks)
+
+            # v is a set of multiplicities, so loop over it and print up to the
+            # 5 shortest smiles for each one. mol_res[k][i] is a set of smiles
+            # corresponding to k (a parameter ID) and i (a single multiplicity)
+            for i in v:
+                smiles = list(sorted(mol_res[k][i], key=lambda s: len(s)))
+                for smile in smiles[:5]:
+                    print("\t", i, smile)
 
 
 if __name__ == "__main__":

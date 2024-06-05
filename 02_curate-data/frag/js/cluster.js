a24340ac8082a32b77bc1b15c87becc55a623ba7
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-05 11:27:03 -0400

    big refactor in mol editor, highlight atoms

--- 02_curate-data/frag/js/cluster.js
+++ 02_curate-data/frag/js/cluster.js
@@ -54,6 +54,147 @@ function centerText(ctx, text, x, y, font_size) {
 	ctx.fillText(text, x - w / 2, y + font_size / 2);
 }
 
+class Circle {
+	constructor(x, y, r) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+
+}
+
+class Atom {
+	constructor(x, y, symbol, charge) {
+		this.x = x;
+		this.y = y;
+		if (charge == 0) {
+			this.symbol = symbol;
+		} else if (charge == -1) {
+			this.symbol = symbol + "-"
+		} else if (charge == 1) {
+			this.symbol = symbol + "+";
+		} else {
+			console.log("warning: unrecognized atomic charge: ", charge);
+		}
+		this.is_highlighted = false;
+	}
+
+	draw(ctx, font_size) {
+		if (this.symbol !== "C") {
+			let w = ctx.measureText(this.symbol).width;
+			ctx.fillText(this.symbol, this.x - w / 2, this.y + font_size / 2);
+		}
+		if (this.is_highlighted) {
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, 1.0 * font_size, 0, 2 * Math.PI);
+			ctx.stroke();
+		}
+	}
+
+	contains(x, y, r) {
+		let dx = x - this.x;
+		let dy = y - this.y;
+		return dx * dx + dy * dy <= r * r;
+	}
+}
+
+class Bond {
+	constructor(x1, y1, x2, y2, order, hl_bond) {
+		let dx = x2 - x1;
+		let dy = y2 - y1;
+		let m = Math.sqrt(dx * dx + dy * dy);
+		let [ux, uy] = [dx / m, dy / m];
+		let [px, py] = [-uy, ux];
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.ux = ux;
+		this.uy = uy;
+		this.px = px;
+		this.py = py;
+		this.order = order;
+		this.hl_bond = hl_bond;
+	}
+
+	draw(ctx) {
+		const f = 2;
+		if (this.hl_bond) {
+			ctx.strokeStyle = "orange";
+		}
+		if (this.order == 1) {
+			ctx.beginPath();
+			ctx.moveTo(this.x1, this.y1);
+			ctx.lineTo(this.x2, this.y2);
+			ctx.stroke();
+		} else if (this.order === 2) {
+			ctx.beginPath();
+			ctx.moveTo(this.x1 + f * this.px, this.y1 + f * this.py);
+			ctx.lineTo(this.x2 + f * this.px, this.y2 + f * this.py);
+			ctx.stroke();
+
+			ctx.beginPath();
+			ctx.moveTo(this.x1 - f * this.px, this.y1 - f * this.py);
+			ctx.lineTo(this.x2 - f * this.px, this.y2 - f * this.py);
+			ctx.stroke();
+		} else {
+			console.log("warning: unknown bond order ", this.order);
+			ctx.strokeStyle = "red";
+			ctx.beginPath();
+			ctx.moveTo(this.x1, this.y1);
+			ctx.lineTo(this.x2, this.y2);
+			ctx.stroke();
+		}
+		ctx.strokeStyle = "black";
+	}
+}
+
+class Scene {
+	constructor(mol) {
+		this.atoms = Array();
+		for (let i = 0; i < mol.atoms.length; i++) {
+			let x = mol.coords[i][0];
+			let y = mol.coords[i][1];
+			let [atomic_sym, charge] = mol.atoms[i];
+			this.atoms.push(new Atom(x, y, atomic_sym, charge));
+		}
+
+		this.bonds = Array();
+		let pad = 8; // padding for ends of bonds
+		for (let [a1, a2, order] of mol.bonds) {
+			let hl_bond = mol.hl_atoms.includes(a1) && mol.hl_atoms.includes(a2);
+			let [x1, y1] = mol.coords[a1];
+			let [x2, y2] = mol.coords[a2];
+			let dx = x2 - x1;
+			let dy = y2 - y1;
+			let m = Math.sqrt(dx * dx + dy * dy);
+			let [ux, uy] = [dx / m, dy / m];
+
+			let [as1, ch1] = mol.atoms[a1];
+			let [as2, ch2] = mol.atoms[a2];
+			if (ch1 != 0 || as1 != "C") {
+				x1 += pad * ux;
+				y1 += pad * uy;
+			}
+			if (ch2 != 0 || as2 != "C") {
+				x2 -= pad * ux;
+				y2 -= pad * uy;
+			}
+			this.bonds.push(new Bond(x1, y1, x2, y2, order, hl_bond));
+		}
+	}
+
+	draw(ctx, font_size) {
+		for (let atom of this.atoms) {
+			atom.draw(ctx, font_size);
+		}
+		for (let bond of this.bonds) {
+			bond.draw(ctx);
+		}
+	}
+}
+
+
 function drawMolecule(mol) {
 	let dialog = document.getElementById("edit-molecule-modal");
 
@@ -69,75 +210,18 @@ function drawMolecule(mol) {
 	ctx.font = `normal ${font_size}px sans-serif`;
 	ctx.fillStyle = "black";
 	ctx.lineWidth = 2;
-	for (let i = 0; i < mol.atoms.length; i++) {
-		let x = mol.coords[i][0];
-		let y = mol.coords[i][1];
-		let [atomic_sym, charge] = mol.atoms[i];
-		if (charge != 0 || atomic_sym != "C") {
-			if (charge == 0) {
-				centerText(ctx, atomic_sym, x, y, font_size);
-			} else if (charge == -1) {
-				centerText(ctx, atomic_sym + "-", x, y, font_size);
-			} else if (charge == 1) {
-				centerText(ctx, atomic_sym + "+", x, y, font_size);
-			} else {
-				console.log("warning: unrecognized atomic charge: ", charge);
-			}
-		}
-	}
 
-	for ([a1, a2, order] of mol.bonds) {
-		let hl_bond = mol.hl_atoms.includes(a1) && mol.hl_atoms.includes(a2);
-		let [x1, y1] = mol.coords[a1];
-		let [x2, y2] = mol.coords[a2];
-		let dx = x2 - x1;
-		let dy = y2 - y1;
-		let m = Math.sqrt(dx * dx + dy * dy);
-		let [ux, uy] = [dx / m, dy / m];
+	let scene = new Scene(mol);
 
-		let [as1, ch1] = mol.atoms[a1];
-		let [as2, ch2] = mol.atoms[a2];
-		let pad = 8;
-		if (ch1 != 0 || as1 != "C") {
-			x1 += pad * ux;
-			y1 += pad * uy;
-		}
-		if (ch2 != 0 || as2 != "C") {
-			x2 -= pad * ux;
-			y2 -= pad * uy;
-		}
-		// perpendicular unit vector to move along for double and triple bonds
-		let [px, py] = [-uy, ux];
-		if (hl_bond) {
-			ctx.strokeStyle = "orange";
-		}
-		if (order == 1) {
-			ctx.beginPath();
-			ctx.moveTo(x1, y1);
-			ctx.lineTo(x2, y2);
-			ctx.stroke();
-		} else if (order === 2) {
-			let f = 2;
+	scene.draw(ctx, font_size);
 
-			ctx.beginPath();
-			ctx.moveTo(x1 + f * px, y1 + f * py);
-			ctx.lineTo(x2 + f * px, y2 + f * py);
-			ctx.stroke();
-
-			ctx.beginPath();
-			ctx.moveTo(x1 - f * px, y1 - f * py);
-			ctx.lineTo(x2 - f * px, y2 - f * py);
-			ctx.stroke();
-		} else {
-			console.log("warning: unknown bond order ", order);
-			ctx.strokeStyle = "red";
-			ctx.beginPath();
-			ctx.moveTo(x1, y1);
-			ctx.lineTo(x2, y2);
-			ctx.stroke();
+	canvas.addEventListener("mousemove", (event) => {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		for (let atom of scene.atoms) {
+			atom.is_highlighted = atom.contains(event.offsetX, event.offsetY, font_size);
 		}
-		ctx.strokeStyle = "black";
-	}
+		scene.draw(ctx, font_size);
+	});
 
 	frame.appendChild(canvas);
 

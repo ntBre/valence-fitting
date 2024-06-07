a61411a4d5676aa7e87e03c131afb203632e98ff
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-07 12:27:40 -0400

    add sql for adding inchikey to molecules table

---
+++ 02_curate-data/frag/sql/add_inchikey.sql
@@ -0,0 +1,17 @@
+ALTER TABLE molecules RENAME TO old_table;
+
+CREATE TABLE molecules
+(
+id integer primary key,
+smiles text unique,
+inchikey text unique,
+natoms int,
+elements blob,
+tag text
+);
+
+INSERT INTO molecules id, smiles, inchikey, natoms, elements, tag
+SELECT id, smiles, smiles, natoms, elements, tag
+FROM old_table;
+
+DROP TABLE old_table;

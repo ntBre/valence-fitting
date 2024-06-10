93de36a11d1a889eda90beae1f716a5d3e2bffaa
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-10 16:23:02 -0400

    try charging molecules in the dataset, report failures at end

--- 02_curate-data/frag/check_dataset.py
+++ 02_curate-data/frag/check_dataset.py
@@ -1,13 +1,17 @@
+import logging
 import re
 
 from openff.toolkit import ForceField, Molecule
 
 from serve import ffname
 
+logging.getLogger("openff").setLevel(logging.ERROR)
+
 ff = ForceField(ffname)
 
 junk = re.compile("[(,)]")
 
+conformers = 0
 with open("dataset.smi") as inp:
     for line in inp:
         pid, cmiles, *rest = line.split()
@@ -20,3 +24,12 @@ with open("dataset.smi") as inp:
                 "ProperTorsions"
             ].values()
         ]
+        try:
+            mol.assign_partial_charges("am1bccelf10")
+        except ValueError as e:
+            if "Omega conformer generation failed" in str(e):
+                conformers += 1
+            else:
+                raise e
+
+print(f"{conformers} failed for omega errors")

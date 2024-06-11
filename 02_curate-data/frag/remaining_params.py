a9e747553668221f6707a3d86294f73daa50b918
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-11 16:36:25 -0400

    update remaining.dat with smirks

--- 02_curate-data/frag/remaining_params.py
+++ 02_curate-data/frag/remaining_params.py
@@ -1,9 +1,16 @@
+from openff.toolkit import ForceField
+
+from serve import ffname
+
 with open("want.params") as inp:
     want = {line.strip() for line in inp}
 
 with open("dataset.smi") as inp:
     got = {line.strip().split()[0] for line in inp}
 
+
+ff = ForceField(ffname)
+tors = ff.get_parameter_handler("ProperTorsions")
 with open("remaining.dat", "w") as out:
     for p in want - got:
-        print(p, file=out)
+        print(p, tors.get_parameter(dict(id=p))[0].smirks, file=out)

4d9f2f7b64318d76f86c78caaa0775a2a7a6a870
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-18 12:29:24 -0400

    compute remaining opt parameters

--- 02_curate-data/frag/remaining_params.py
+++ 02_curate-data/frag/remaining_params.py
@@ -2,15 +2,15 @@ from openff.toolkit import ForceField
 
 from serve import ffname
 
-with open("want.params") as inp:
+with open("want.opt") as inp:
     want = {line.strip() for line in inp}
 
-with open("dataset.smi") as inp:
+with open("opt.smi") as inp:
     got = {line.strip().split()[0] for line in inp}
 
 
 ff = ForceField(ffname)
 tors = ff.get_parameter_handler("ProperTorsions")
-with open("remaining.dat", "w") as out:
+with open("remaining.opt", "w") as out:
     for p in want - got:
         print(p, tors.get_parameter(dict(id=p))[0].smirks, file=out)
